81516c5a53c797e32818e5328176fec0c06684c3
carvalhoisaac22 <carvalhoisaac2021@gmail.com>
2023-05-05 19:01:06 -0300

    Testando Forms com o horário do servidor

--- agronorte/app/urls.py
+++ agronorte/app/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import horario
+from .views import horario, form_cliente
+
 
 urlpatterns = [
-    path('horario',horario)
+    path('horario',horario),
+    path('form_cliente', form_cliente ),
     
 
 ]
--- agronorte/app/views.py
+++ agronorte/app/views.py
@@ -1,9 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 import datetime
+from .forms import ClienteForm
 
 # Create your views here.
 
 def horario (request):
     horario = datetime.datetime.now()
-    return HttpResponse(horario)  
+    return render(request, 'lista_horario.html', context={'horario':horario})
+
+
+def form_cliente(request):
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('horario')
+        form = ClienteForm()
+    return render(request, 'form_cliente.html', context={'form': form})
