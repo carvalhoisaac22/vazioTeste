19254ffc16e631051cc557f2575e0e64191e2202
carvalhoisaac22 <carvalhoisaac2021@gmail.com>
2023-05-08 18:33:28 -0300

    Correção da estrutura do projeto

--- agronorte/urls.py
+++ agronorte/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 from app_site.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home)
+    path('', home)
 ]
--- app_site/admin.py
+++ app_site/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from app_site.models import Categoria, Transacao
+
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Transacao)
