6c916a17152d97baf60192af00afb5eb7db222ca
carvalhoisaac22 <carvalhoisaac2021@gmail.com>
2023-05-08 18:51:16 -0300

    NUTRICAO: Import do html PURO para o Django

--- agronorte/urls.py
+++ agronorte/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app_site.views import home
+from app_site.views import home, nutricao
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
+    path('nutricao/',nutricao)
 ]
