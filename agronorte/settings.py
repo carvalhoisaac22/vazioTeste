b716ee6affb55b288c3395bcfcdef6d778eacfb9
carvalhoisaac22 <carvalhoisaac2021@gmail.com>
2023-05-09 17:28:33 -0300

    test

--- agronorte/settings.py
+++ agronorte/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#t4i4c!23^nccx@@zy2t^g5_wj4(w=rwwvmq^m!u68=3iz2cak
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [['192.168.0.6', 'localhost']]
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_site',
-    'livereload',
+    
 ]
 
 MIDDLEWARE = [
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'agronorte.urls'
@@ -111,6 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
